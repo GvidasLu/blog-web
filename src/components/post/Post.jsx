a0f50105fb329e07d2edd3cd0c70d42fe508e18b
GvidasLu <luspinis.gvidas@gmail.com>
2022-03-02 08:43:14 +0200

    added button; added new files

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,4 +1,12 @@
+import React, {useState} from 'react';
+
 const Post = (props) => {
+    const [readStatus, setReadStatus] = useState('New');
+    const changeStatusHandler = () => {
+        setReadStatus('Read done');
+    }
+
+    // const userStatus = userLogedIn ? 'online':'offline';
     return (
         <div class="col-lg-6 col-xxl-4 mb-5">
                         <div class="card bg-light border-0 h-100">
@@ -6,6 +14,11 @@ const Post = (props) => {
                                 <div class="feature bg-primary bg-gradient text-white rounded-3 mb-4 mt-n4"><i class="bi bi-collection"></i></div>
                                 <h2 class="fs-4 fw-bold">{props.title}</h2>
                                 <p class="mb-0">{props.content}</p>
+                                {readStatus === 'Read done' ?
+                                    <p>Naujiena perskaityta</p>:  //true
+                                    <p>Nauujiena neperskaityta</p> //false
+                                }
+                                <button onClick={changeStatusHandler}>{readStatus}</button>
                             </div>
                         </div>
                     </div>
